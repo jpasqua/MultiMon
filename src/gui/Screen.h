36b08616c63bfe3e785cd946a44428fd8ef01490
Joe Pasqua <joe@NoRoomAtTheInn.org>
2020-06-10 17:15:27 -0700

    debounce screen taps

--- src/gui/Screen.h
+++ src/gui/Screen.h
@@ -22,8 +22,7 @@ public:
 
   // ----- Functions that may be overriden by subclasses
   virtual void activate() {
-    wasPressed = false;
-    startOfPress = 0;
+    startOfPress = endOfPress = 0;
     display(true);
   }
 
@@ -31,31 +30,38 @@ public:
     uint16_t tx = 0, ty = 0;
     bool pressed = GUI::getTouch(&tx, &ty);
     if (pressed) {
-      if (!wasPressed) {
-        wasPressed = true;
+      if (startOfPress == 0) {
         startOfPress = millis();
+        endOfPress = 0;
       }
       lastX = tx; lastY = ty;
     } else {
-      if (wasPressed) {
-        // Ok, we got a press/release, see which button (if any) is associated
-        Button::PressType pt;
-        uint32_t pressDuration = (millis() - startOfPress);
-        if (pressDuration >= Button::VeryLongPressInterval) pt = Button::PressType::VeryLongPress;
-        else if (pressDuration >= Button::LongPressInterval) pt = Button::PressType::LongPress;
-        else pt = Button::PressType::NormalPress;
+      if (startOfPress != 0) {
+        if (endOfPress == 0) {
+          endOfPress = millis();
+        } else if (millis() - endOfPress > DebounceTime) {
+          // Process the press
+          // Ok, we got a press/release, see which button (if any) is associated
+          Button::PressType pt;
+          uint32_t pressDuration = (millis() - startOfPress);
+          if (pressDuration >= Button::VeryLongPressInterval) pt = Button::PressType::VeryLongPress;
+          else if (pressDuration >= Button::LongPressInterval) pt = Button::PressType::LongPress;
+          else pt = Button::PressType::NormalPress;
 
-        for (int i = 0; i < nButtons; i++) {
-          if (buttons[i].processTouch(lastX, lastY, pt)) break;
+          for (int i = 0; i < nButtons; i++) {
+            if (buttons[i].processTouch(lastX, lastY, pt)) break;
+          }
+          endOfPress = 0;
+          startOfPress = 0;
         }
       }
-      wasPressed = false;
     }
   }
 
 private:
-  bool wasPressed;
-  uint32_t startOfPress;
+  static const uint32_t DebounceTime = 50;
+  uint32_t startOfPress = 0;
+  uint32_t endOfPress = 0;
   uint16_t lastX, lastY;
 };
 
